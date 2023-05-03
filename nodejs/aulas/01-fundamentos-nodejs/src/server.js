cc2403150afa0ba5d8328491b1f477d54a45cf82
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-05-03 12:35:25 -0300

    feat: :sparkles: exibindo mensagem em decorrer do method e url

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,24 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  
+  if (method === 'GET' && url === '/users') {
+      res.end('Listagem de usuários')
+  }
+  if (method === "POST" && url === "/users") {
+      res.end("Criação de usuários");
+  }
+  if (method === "PUT" && url === "/users") {
+      res.end("Alteração de usuários");
+  }
+  if (method === "PATCH" && url === "/users") {
+      res.end("Alterção específica de usuários");
+  }
+  if (method === "DELETE" && url === "/users") {
+      res.end("Deleção de usuários");
+  }
+
   return res.end('Hello World!');
 });
 
