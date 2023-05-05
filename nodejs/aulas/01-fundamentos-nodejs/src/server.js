5d3893875dff77a7fcee2703b458ca8aee3f6994
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-05-05 11:57:18 -0300

    feat: :sparkles: corpo da requisição em json - stream e buffers

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -2,8 +2,22 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  console.log(req.body);
 
   //console.log(req.headers)
   
@@ -14,10 +28,11 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
     users.push({
       id: 1,
-      name: 'John Duo',
-      email: 'jhonduo@email.com'
+      name,
+      email
     });
     return res.writeHead(201).end();
   }
