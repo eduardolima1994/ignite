84640e77a3aa0e30d30700bbda8c13f078cd88d1
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-05-08 13:32:05 -0300

    feat: :sparkles: extração de query params usando regex

--- nodejs/aulas/01-fundamentos-nodejs/src/routes.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/routes.js
@@ -9,6 +9,7 @@ export const routes = [
         method: "GET",
         path: buildRoutePath("/users"),
         handler: (req, res) => {
+            console.log(req.query);
             const users = database.select("users");
             return res.end(JSON.stringify(users));
         },
@@ -35,8 +36,8 @@ export const routes = [
             const { id } = req.params;
             const { name, email } = req.body;
             database.update("users", id, {
-              name,
-              email
+                name,
+                email,
             });
             return res.writeHead(204).end();
         },
