d0bad4345fec93f623779971e54e9de4f957b74a
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-03 14:29:44 -0300

    refactor: :bug: correção de rota onde faltou uma /

--- nodejs/aulas/03-api-solid/src/http/routes.ts
+++ nodejs/aulas/03-api-solid/src/http/routes.ts
@@ -7,5 +7,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  app.get('me', profile)
+  app.get('/me', profile)
 }
