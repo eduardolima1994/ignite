6f5a7d7e51b1628ae88b7baeccfa5d96e887c777
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-06 13:42:37 -0300

    refactor: :zap: melhoria no contoller de usuários

--- nodejs/aulas/03-api-solid/src/http/controllers/users/routes.ts
+++ nodejs/aulas/03-api-solid/src/http/controllers/users/routes.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from 'fastify'
 
-import { verifyJWT } from '../../middlewares/verify-jwt'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
