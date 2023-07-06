357d3af3dc08bbeec2159c78c6f9dc20254b96dc
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-06 13:03:28 -0300

    feat: :zap: melhoria na estrutura de arquivos e pastas de controllers

--- nodejs/aulas/03-api-solid/src/http/controllers/users/routes.ts
+++ nodejs/aulas/03-api-solid/src/http/controllers/users/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './controllers/register'
-import { authenticate } from './controllers/authenticate'
-import { profile } from './controllers/profile'
-import { verifyJWT } from './middlewares/verify-jwt'
+
+import { verifyJWT } from '../../middlewares/verify-jwt'
+
+import { register } from './register'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
