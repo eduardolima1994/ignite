1b36269500ca62387b05acedc13799aa1e88c15d
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-11 15:03:38 -0300

    test: :sparkles: criação de teste end-to-end para criação de academias

---
+++ nodejs/aulas/03-api-solid/src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description.',
+        phone: '75999999999',
+        latitude: -9.3938008,
+        longitude: -37.9890493,
+      })
+
+    expect(response.status).toEqual(201)
+  })
+})
