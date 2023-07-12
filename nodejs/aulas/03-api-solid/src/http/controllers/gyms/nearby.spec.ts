de0e8755821a87414db6b27e01841fd690448264
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-12 15:14:17 -0300

    feat: :zap: criação de teste para funcionalidade de academia

---
+++ nodejs/aulas/03-api-solid/src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Nearby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description.',
+        phone: '75999999999',
+        latitude: -9.3938008,
+        longitude: -37.9890493,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Some description.',
+        phone: '75999999999',
+        latitude: -9.3938008,
+        longitude: -39.1890793,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -9.3938008,
+        longitude: -37.9890493,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    ])
+  })
+})
