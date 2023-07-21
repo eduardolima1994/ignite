f90f559f6118ed62064c38cded85d540c85e7cad
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-21 08:28:16 -0300

    feat: :zap: inserindo popriedades nos com componenetes Post

--- reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -3,11 +3,14 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Eduardo Lima"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, maxime cum. "
+      />
+      <Post
+        author="Jhon Duo"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, maxime cum. "
+      />
     </div>
   )
 }
