edba8e1785b72a410d11eb02bcd80b4608dd0ae3
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-21 12:35:16 -0300

    feat: :zap: criação da sidebar

---
+++ reactjs/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+      />
+    
+      <div className={styles.profile}>
+        <strong>Eduardo Lima</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
