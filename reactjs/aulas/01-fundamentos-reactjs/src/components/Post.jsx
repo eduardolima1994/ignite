6adaf15dcca6bf0de6cb3254f64b8f96714780de
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-24 10:48:18 -0300

    feat: :zap: estrutura html e react para postagem

---
+++ reactjs/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/55820803?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Eduardo Lima</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="24 de Julho às 08:13h" dateTime="2023-07-24 10:30:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 🙌</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare</p>
+        
+        <p>🚀 <a href="#">jane.design/doctorcare</a></p>
+        
+        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
