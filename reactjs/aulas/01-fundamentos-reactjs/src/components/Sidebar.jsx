f52da4290da632478ac76667c237ed3e1e39fea5
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-26 10:45:19 -0300

    feat: :art: mudança de imagem do perfil

--- reactjs/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ reactjs/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ export function Sidebar() {
       />
     
       <div className={styles.profile}>
-        <Avatar src="https://avatars.githubusercontent.com/u/97980251?v=4" />
+        <Avatar src="https://encurtador.com.br/mqvSX" />
 
         <strong>Eduardo Lima</strong>
         <span>Web Developer</span>
