5a547ca1a587ee43110510afd4bf656c34f4eda3
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-26 10:46:14 -0300

    feat: :zap: criando array de objetos simulando dados consumidos do backend

--- reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/55820803?v=4',
+      name: 'Eduardo Lima',
+      role: 'Dev @PMDG'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 🙌' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type: 'link', content: 'jane.design/doctorcare' },        
+    ],
+    publishedAt: new Date('2023-07-20 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://encurtador.com.br/psvLQ',
+      name: 'Tifany Souza',
+      role: 'Esteticista @Emagrecentro'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 🙌' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type: 'link', content: 'jane.design/doctorcare' },        
+    ],
+    publishedAt: new Date('2023-07-10 20:00:00')
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -16,14 +47,15 @@ export function App() {
           <Sidebar />
         </aside>
         <main>
-          <Post
-            author="Eduardo Lima"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, maxime cum. "
-          />
-          <Post
-            author="Jhon Duo"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, maxime cum. "
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
