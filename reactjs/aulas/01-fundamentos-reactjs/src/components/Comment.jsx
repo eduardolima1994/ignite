e8ec8c9e537e6e20e7fed472b8f38f31abe5df07
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-07-27 14:09:25 -0300

    feat: :zap: passando função como propriedade de componente filho do react

--- reactjs/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ reactjs/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/55820803?v=4" alt='' />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
               <time title="24 de Julho às 08:13h" dateTime="2023-07-24 10:30:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
