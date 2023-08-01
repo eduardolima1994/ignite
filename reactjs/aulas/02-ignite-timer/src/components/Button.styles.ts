ae6eeeed3ea9b4e53222a98dd9b5366968c5aca2
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-08-01 14:28:18 -0300

    feat: :art: atribuindo propriedades css vindas do tema padrão

--- reactjs/aulas/02-ignite-timer/src/components/Button.styles.ts
+++ reactjs/aulas/02-ignite-timer/src/components/Button.styles.ts
@@ -17,9 +17,17 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  /*
   ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]}
     `
   }}
+  */
 `
