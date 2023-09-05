1ec368f0bcd543bd471ae35c428b9dc1f28c6913
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-09-05 13:37:25 -0300

    feat: :zap: criando tabela de transações

--- reactjs/aulas/03-dt-money/src/pages/Transactions/index.tsx
+++ reactjs/aulas/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,32 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContainer } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <table>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>04/09/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>- R$ 7.000,00</td>
+              <td>Alimentação</td>
+              <td>01/09/2023</td>
+            </tr>
+            
+          </tbody>
+        </table>
+      </TransactionsContainer>
     </div>
   )
 } 
