a93668b01908602163ce0e84171453e0617bd9b4
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-09-11 14:34:30 -0300

    feat: importação do Dialog para novo componente

--- reactjs/aulas/03-dt-money/src/components/Header/index.tsx
+++ reactjs/aulas/03-dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../../assets/logo.svg';
 import { NewTransactionModal } from "../NewTransactionModal";
